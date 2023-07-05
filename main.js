80239115024356f42a8eee66d7fd33a2fa9cb3ca
Ashley Akins <ashleyakins2015@hotmail.com>
2023-07-05 05:42:50 -0500

    update

--- main.js
+++ main.js
@@ -2,11 +2,32 @@
 - Objective box will not be manipulated with js 
 GuessBx will be an array*/
 
+/*-------------Variables-----------------*/
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+          't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let wCat;         // Array of topics
 let selectdCat;     // Selected catagory
 let getHint ;          // Word getHint
 let selectdWord ;      // Selected word
 let guess ;             // Guess
-let stordGuesses = [ ];      // Stored geusses
+let stordGuesses = [ ]; // Stored guesses
 let lives;             // Lives
 let space;              // Number of spaces in word '-'
+
+/*-------------cached elements-----------------*/
+
+
+
+
+
+/*-------------event listeners-----------------*/
+
+
+
+
+/*-------------fucntions-----------------*/
+
+
+
+
