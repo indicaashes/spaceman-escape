ebca2aea0b3542883ed7225d2aa02df1bcc5d337
Ashley Akins <ashleyakins2015@hotmail.com>
2023-07-05 04:34:34 -0500

    update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 /* - WCat is going to be an array
 - Objective box will not be manipulated with js 
 GuessBx will be an array*/
+
+let wCat;         // Array of topics
+let selectdCat;     // Selected catagory
+let getHint ;          // Word getHint
+let selectdWord ;      // Selected word
+let guess ;             // Guess
+let stordGuesses = [ ];      // Stored geusses
+let lives;             // Lives
+let space;              // Number of spaces in word '-'
